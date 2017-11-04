87f77b0c7246c8f49aeafc8a5e91398740a91a93
marcelolynch <mlynch@itba.edu.ar>
2017-11-04 02:54:04 -0300

    Fix de la llamada asincronica

--- web/scripts/map.js
+++ web/scripts/map.js
@@ -10,41 +10,42 @@ angular.module('mappinApp', ['ngAnimate'])
 			 tabs: [
 				 {name: "1920", id:1, 
 				  pins:[
-					  {name: "Hipolito Yrigoyen presidente", description:"De la UCR", latlng: {lat: 10.40, lng:-43.23}, type:"nature"},
-					  {name: "Torcuato de Alvear", description:"De la UCR", latlng: {lat: -10.40, lng:33.23}, type:"art"}]}, 
+					  {name: "Hipolito Yrigoyen presidente", description:"De la UCR", latlng: {lat: 10.40, lng:-43.23}, type:"nature", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"},
+					  {name: "Torcuato de Alvear", description:"De la UCR", latlng: {lat: -10.40, lng:33.23}, type:"art", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"}]}, 
 				 {name: "1930", id:2,
 				  pins:[
-					  {name: "Uriburu", description:"dictador de facto", latlng: {lat: 90.40, lng:-3.23}, type:"religion"},{name: "Pedro Justo", description:"Partido democrata nacional", latlng: {lat: -40.40, lng:93.23}, type:"science"}]},
+					  {name: "Uriburu", description:"dictador de facto", latlng: {lat: 90.40, lng:-3.23}, type:"religion", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"},{name: "Pedro Justo", description:"Partido democrata nacional", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg", latlng: {lat: -40.40, lng:93.23}, type:"science"}]},
 				 {name: "1940", id:3, 
 				  pins:[
-					  {name: "Hipolito Yrigoyen presidente", description:"De la UCR", latlng: {lat: 10.40, lng:-43.23}, type:"religion"},
-					  {name: "Torcuato de Alvear", description:"De la UCR", latlng: {lat: -10.40, lng:33.23}, type:"art"}]}, 
+					  {name: "Hipolito Yrigoyen presidente", description:"De la UCR", latlng: {lat: 10.40, lng:-43.23}, type:"religion", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"},
+					  {name: "Torcuato de Alvear", description:"De la UCR", latlng: {lat: -10.40, lng:33.23}, type:"art", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"}]}, 
 				 {name: "1950", id:4,
 				  pins:[
-					  {name: "Uriburu", description:"dictador de facto", latlng: {lat: 90.40, lng:-3.23}, type:"politics"},
-					  {name: "Pedro Justo", description:"Partido democrata nacional", latlng: {lat: -40.40, lng:93.23}, type: "society"}]},
+					  {name: "Uriburu", description:"dictador de facto", latlng: {lat: 90.40, lng:-3.23}, type:"politics", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"},
+					  {name: "Pedro Justo", description:"Partido democrata nacional", latlng: {lat: -40.40, lng:93.23}, type: "society", img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"}]},
 			 ]
 			}
 		;
 	
 
-  this.infomap.tabs[0].pins.forEach(function(item, index){
-          addPin(item);
-      })
-
-
   this.showDescription = false;
 	
 	this.titleHover = function(value) {
   this.showDescription = value;
-	
   	}
+
+
+    this.infomap.tabs[0].pins.forEach(function(item, index){
+            addPin(item);
+    })
+
+  
 	})
 ;
 
 //$('#myModal').modal({ show: false})
 
-    var selectedLatLng = {lat: 0, lng:0};
+  var selectedLatLng = {lat: 0, lng:0};
 
   function markerCreate() {
 
@@ -55,7 +56,8 @@ angular.module('mappinApp', ['ngAnimate'])
       var pin = {
         name: hitoTitle,
         description: desc,
-        latlng: selectedLatLng
+        latlng: selectedLatLng,
+        img_url: "http://assets.vg247.com/current//2015/06/the_witcher_3_close_up_geralt_hrrr.jpg"
       }     
       
       console.log("Esto se llama")
@@ -66,13 +68,12 @@ angular.module('mappinApp', ['ngAnimate'])
 
 
     function addPin(pin){
-            console.log("Esto se llama2 ")
-
+      console.log("Calling me")
         var contentString = '<div id="iw-container">' +
                     '<div class="iw-title">'+ pin.name + '</div>' +
                     '<div class="iw-content">' +
                       '<div class="iw-subTitle">Descripción</div>' +
-                      '<img src="http://maps.marnoto.com/en/5wayscustomizeinfowindow/images/vistalegre.jpg" alt="Porcelain Factory of Vista Alegre" height="115" width="83">' +
+                      '<img src="'+ pin.img_url +'">' +
                       '<p>'+ pin.description + '</p>'+
                     '</div>' +
                     '<div class="iw-bottom-gradient"></div>' +
@@ -124,123 +125,3 @@ angular.module('mappinApp', ['ngAnimate'])
 
     }
 
-
-    function initMap() {
-      var uluru = {lat: -33, lng: -55};
-
-       map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
-          center: uluru,
-          mapTypeControl: false,
-          fullscreenControl: false,
-          streetViewControl: false,
-          styles: [
-                    {
-                      "featureType": "administrative.land_parcel",
-                      "elementType": "geometry",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    },
-                    {
-                      "featureType": "administrative.neighborhood",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    },
-                    {
-                      "featureType": "administrative.neighborhood",
-                      "elementType": "geometry.fill",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    },
-                    {
-                      "featureType": "landscape.man_made",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    },
-                    {
-                      "featureType": "poi",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    },
-                    {
-                      "featureType": "road",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    },
-                    {
-                      "featureType": "transit",
-                      "stylers": [
-                        {
-                          "visibility": "off"
-                        }
-                      ]
-                    }
-                  ]
-        });
-          
-          var input = /** @type {!HTMLInputElement} */(document.getElementById('autocomplete'));
-
-        var types = document.getElementById('type-selector');
-
-        var autocomplete = new google.maps.places.Autocomplete(input, {types: ['(cities)']});
-        autocomplete.bindTo('bounds', map);
-
-        var infowindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
-          map: map,
-          anchorPoint: new google.maps.Point(0, -29)
-        });
-
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
-
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-          }
-        
-        });
-
-      map.addListener('rightclick', function(mouseEvent) {
-          selectedLatLng = mouseEvent.latLng;
-          
-          // Clear values
-          $("#hito").val('');
-          $("#hitodesc").val('');
-
-          $('#marker-modal').modal('show')
-     });
-
-
-
-
-      }
