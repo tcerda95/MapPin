4e190e7bde30308ca6b3de65fd28955d56701cb9
Luciano Bianchi <lucianogbianchi@gmail.com>
2017-11-04 05:44:50 -0300

    estrellitas dummy

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,18 +1,15 @@
-function initMap() {
-	// Create a map object and specify the DOM element for display.
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
-};
-
 angular.module('mappinApp', [])
-	.controller('HomeController', function() {
-		this.maps = [
-			{name: "Historia Argentina 1810", description: "Una mapa con una descripcion muy larga que va a hacer que tenga que poner tres puntitos para marcar que continuasdkj skjgnkjfg fgs dgdsfgdf"},
-			{name: "Migraciones prehistóricas", description: "El otro mapa no tiene descripcion"},
-			{name: "Mappin hola"}
-		];
+	.controller('HomeController', function($http) {
+		$http.get('http://localhost:8080/map').
+        then(function(response) {
+			this.maps = response.data;
+			console.log(this.maps);
+        });
+//		this.maps = [
+//			{name: "Historia Argentina 1810", description: "Una mapa con una descripcion muy larga que va a hacer que tenga que poner tres puntitos para marcar que continuasdkj skjgnkjfg fgs dgdsfgdf"},
+//			{name: "Migraciones prehistóricas", description: "El otro mapa no tiene descripcion"},
+//			{name: "Mappin hola"}
+//		];
 	})
 ;
 
